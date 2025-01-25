4559919f5ee2f1fa8909455005c70f11afa63ffc
Grybos Kamil <grybos.kamil@gmail.com>
2025-01-25 01:35:10 +0100

    Clean styles.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import Chat from "./components/Chat";
 
+import "./App.css";
+
 export default function App() {
   return (
-    <div>
+    <div id="App">
       <h1>Live Chat</h1>
       <Chat />
     </div>
