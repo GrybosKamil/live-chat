8d7bac5d9979efe12774caab7bb479ab9b49492c
Grybos Kamil <grybos.kamil@gmail.com>
2025-01-26 13:59:21 +0100

    Change socket path. Serve client app from server address.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,28 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
-import { setupSocket } from './socket';
+import express from "express";
+import http from "http";
+import path from "path";
+import { Server } from "socket.io";
+import { setupSocket } from "./socket";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST"]
-    }
-  });
+  path: "/api/socket",
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
 setupSocket(io);
 
 const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+});
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
